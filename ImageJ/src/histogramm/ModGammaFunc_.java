50ca97251b50664f45124dc5d50793df8b5b34aa
Grisu118 <grisu118@grisu118.ch>
2015-10-03 15:27:30 +0200

    formatting

--- ImageJ/src/histogramm/ModGammaFunc_.java
+++ ImageJ/src/histogramm/ModGammaFunc_.java
@@ -10,7 +10,7 @@ import java.awt.*;
 /**
  * Created by benjamin on 03.10.2015.
  */
-public class ModGammaFunc_ implements PlugInFilter{
+public class ModGammaFunc_ implements PlugInFilter {
     @Override
     public int setup(String s, ImagePlus imagePlus) {
         return DOES_8G;
@@ -26,24 +26,24 @@ public class ModGammaFunc_ implements PlugInFilter{
         int K = 256;
 
         if (sRGB.equals(JOptionPane.showInputDialog(null, "Will You use ITU or sRGB Standart?", "sRGB or ITU?", JOptionPane.QUESTION_MESSAGE, null, new String[]{sRGB, itu}, sRGB))) {
-            gamma = 1/2.4;
+            gamma = 1 / 2.4;
             x0 = 0.00304;
         } else {
             gamma = 1 / 2.222;
             x0 = 0.018;
         }
 
-        final double s = gamma / (x0*(gamma-1) + Math.pow(x0, 1-gamma));
-        final double d = 1 / (Math.pow(x0, gamma)*(gamma-1)+1) - 1;
+        final double s = gamma / (x0 * (gamma - 1) + Math.pow(x0, 1 - gamma));
+        final double d = 1 / (Math.pow(x0, gamma) * (gamma - 1) + 1) - 1;
 
         int[] lookUpTable = new int[K];
 
         for (int i = 0; i < lookUpTable.length; i++) {
-            double x = (double)i/(K-1);
+            double x = (double) i / (K - 1);
             if (x <= x0) {
-                lookUpTable[i] = (int) Math.round(x*s*(K-1));
+                lookUpTable[i] = (int) Math.round(x * s * (K - 1));
             } else {
-                lookUpTable[i] = (int) Math.round(((1 + d) * Math.pow(x, gamma) - d)*(K-1));
+                lookUpTable[i] = (int) Math.round(((1 + d) * Math.pow(x, gamma) - d) * (K - 1));
             }
         }
         imageProcessor.applyTable(lookUpTable);
